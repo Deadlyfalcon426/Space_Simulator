ab672364477609364c52debf1935790089c3b966
Ahsan <ahsan.moh426@gmail.com>
2026-08-22 17:41:15 -0400

    CMake setup

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -10,19 +10,24 @@ signed main() {
     //initial set up
     Vector3 origin = Vector3(0,0,0,"m");
     std::vector<CelestialBody> CelestialBodies;
+    std::vector<Vector3> initial_positions;
     //sun
     CelestialBody sun = CelestialBody("Sun", 1.989*std::pow(10, 30));
     CelestialBodies.push_back(sun);
+    initial_positions.push_back(sun.get_position());
+
     //earth
     CelestialBody earth = CelestialBody("Earth", 5.9722*std::pow(10, 24));
     earth.update_position(1.52087774*std::pow(10, 11),0,0);
     earth.set_velocity(0,0,2.9300*std::pow(10,4));
     CelestialBodies.push_back(earth);
+    initial_positions.push_back(earth.get_position());
     //mars
     CelestialBody mars = CelestialBody("Mars", 6.417*std::pow(10, 23));
     mars.update_position(2.49*std::pow(10, 11),0,0);
     mars.set_velocity(0,0,2.19424229375*std::pow(10, 4));//used vis-viva!
-    //CelestialBodies.push_back(mars);
+    CelestialBodies.push_back(mars);
+    initial_positions.push_back(mars.get_position());
 
     print_heavenly_vector(CelestialBodies);
     //31556952 <- seconds in a year
@@ -34,6 +39,12 @@ signed main() {
     sim(CelestialBodies, time, step, choice);
 
     print_heavenly_vector(CelestialBodies);
+    int counter = 0;
+    for(CelestialBody& body : CelestialBodies){
+        initial_positions[counter] = body.get_position().difference(initial_positions[counter]);
+        std::cout<<"\n"<<initial_positions[counter];
+        counter++;
+    }
     return 0;
 }
 
